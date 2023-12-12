fb7b21728e24cebc4288db543ebc6717ed97cb06
JossPlay14 <josechoque251@gamil.com>
2023-12-12 15:47:16 -0400

    inst

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://JossPlay14.github.io/Instituto-Ensenianza-Idioma"
+  base:"/Instituto-Ensenianza-Idioma/"
 });
